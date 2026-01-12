b58e6c6da6f6c94a945d63a5ff31ffa0a214b64a
Shivam Shinde <shivamshinde92722@gmail.com>
2026-01-11 22:59:31 -0500

    Add logger.py to src/utils/

---
+++ src/utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+# configure logging 
+logger = logging.getLogger("MAL_MCPServer")
+logger.setLevel(logging.DEBUG)
+
+# File handler with DEBUG level
+file_handler = logging.FileHandler("mal_mcp_server.log")
+file_handler.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
